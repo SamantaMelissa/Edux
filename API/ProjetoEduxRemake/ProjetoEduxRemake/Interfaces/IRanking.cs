203e480c578177521d60b039e152e617eb73f422
Samanta Melissa <samanta.melissa@gmail.com>
2020-11-24 18:27:03 -0300

    terminando o ranking

---
+++ API/ProjetoEduxRemake/ProjetoEduxRemake/Interfaces/IRanking.cs
@@ -0,0 +1,15 @@
+﻿using ProjetoEduxRemake.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoEduxRemake.Interfaces
+{
+    public class IRanking
+    {
+        List<Ranking> Listar();
+        Ranking BuscarPorId(Guid id);
+        
+    }
+}
---
+++ API/ProjetoEduxRemake/ProjetoEduxRemake/Repositories/RankingRepository.cs
@@ -0,0 +1,14 @@
+﻿using ProjetoEduxRemake.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoEduxRemake.Repositories
+{
+    public class RankingRepository : IRanking
+    {
+        
+
+    }
+}
